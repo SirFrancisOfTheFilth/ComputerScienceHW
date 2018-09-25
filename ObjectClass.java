8fec98637a9c71b490609f1065dfeb68d4ce4305
SirFrancisOfTheFilth <mtarekegn143@gmail.com>
2018-09-24 20:45:56 -0400

    Add files via upload

--- ObjectClass.java
+++ ObjectClass.java
@@ -1,16 +1,9 @@
-
-/**
- * Write a description of class ObjectClass here.
- *
- * @author (your name)
- * @version (a version number or a date)
- */
 public class ObjectClass
 {
     // instance variables - replace the example below with your own
         String finalStr = "";
         String intermediateStr = "";
-        
+
         int randomNum;
         int capitalizationNum;
 
@@ -19,28 +12,28 @@ public class ObjectClass
      */
     public ObjectClass()
     {
-            
+
     }
 
 
-    public String randomPasswordGenerator(String input){
-        
-        for(int i = 1; i <= 8; i++){
-            
-           randomNum = (int)( (Math.random() * 45) ) + 1;
+    public String randomPasswordGenerator(String input, int val){
+
+        for(int i = 1; i <= val; i++){
+
+           randomNum = (int)( (Math.random() * 44) ) + 1;
            capitalizationNum = (int)( (Math.random() * 8) + 1);
            intermediateStr = input.substring(randomNum, randomNum + 1);
-           
+
            if( (capitalizationNum % 2 == 0) && (randomNum <= 25) ){
                String capitalStr;
                capitalStr = intermediateStr.toUpperCase();
                finalStr += capitalStr;
             } else {
-               finalStr += intermediateStr; 
+               finalStr += intermediateStr;
             }
-            
+
         }
-        
+
         return finalStr;
     }
 }
--- RunnerClass.java
+++ RunnerClass.java
@@ -1,17 +1,17 @@
-
-/**
- * Write a description of class RunnerClass here.
- *
- * @author (your name)
- * @version (a version number or a date)
- */
+import java.util.*;
 public class RunnerClass
 {
     // instance variables - replace the example below with your own
     public static void main(String[] args){
-        ObjectClass obj = new ObjectClass();
-        String returnStr = obj.randomPasswordGenerator("abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*?");
+        ObjectClass obj;
+        System.out.println("Entire a length that you would like your password to be");
+        Scanner newScan = new Scanner(System.in);
+        int scanVal = newScan.nextInt();
+        for(int i = 0; i <= 19; i++){
+        obj = new ObjectClass();
+        String returnStr = obj.randomPasswordGenerator("abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*?",scanVal );
         System.out.println(returnStr);
+        }
     }
-    
+
 }
