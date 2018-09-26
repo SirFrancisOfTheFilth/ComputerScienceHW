1e343d24e3b1865ce944c2cbdaa2de3e75872f28
SirFrancisOfTheFilth <mtarekegn143@gmail.com>
2018-09-25 20:47:24 -0400

    Update Grades.java

--- Grades.java
+++ Grades.java
@@ -15,8 +15,12 @@ public class Grades{
       sumNum += gradeArr[i];
       System.out.println("Grade " + (i + 1) + " : " + gradeArr[i]);
     }
-    System.out.println("The average of the grades with the lowest grade is: " + sumNum/gradeArr.length + ".");
-    System.out.println("The average of the grades without the lowest grade is: " + (sumNum - lowestGrade)/(gradeArr.length - 1) + ".");
+
+    double avg1 = (int)(100 * sumNum/gradeArr.length)/100.0;
+    double avg2 = (int)(100 * (sumNum - lowestGrade)/(gradeArr.length - 1) )/100.0;
+
+    System.out.println("The average of the grades with the lowest grade is: " + avg1 + ".");
+    System.out.println("The average of the grades without the lowest grade is: " + avg2 + ".");
     System.out.println("The lowest grade was " + lowestGrade + ".");
 
   }
