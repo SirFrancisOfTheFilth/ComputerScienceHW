885bcc1fa41cf5c726134c11991acbdd66dc2071
SirFrancisOfTheFilth <mtarekegn143@gmail.com>
2018-10-10 13:01:19 -0400

    Add files via upload

---
+++ Magpie2.java
@@ -0,0 +1,146 @@
+/**
+ * A program to carry on conversations with a human user.
+ * This is the initial version that:  
+ * <ul><li>
+ *       Uses indexOf to find strings
+ * </li><li>
+ *          Handles responding to simple words and phrases 
+ * </li></ul>
+ * This version uses a nested if to handle default responses.
+ * @author Laurie White
+ * @version April 2012
+ */
+public class Magpie2
+{
+    /**
+     * Get a default greeting   
+     * @return a greeting
+     */
+    public String getGreeting()
+    {
+        return "Hello, let's talk.";
+    }
+    
+    /**
+     * Gives a response to a user statement
+     * 
+     * @param statement
+     *            the user statement
+     * @return a response based on the rules given
+     */
+    public String getResponse(String statement)
+    {
+        String[] keywords = {"no", "mother", "father", "sister", "brother", "dog", "cat", "Mr.", "Ms.", "Mrs."};
+        
+        String response = "";
+        if ( keywordPrecedence(keywords, statement).equals("no") && keywordDetection(statement, keywordPrecedence(keywords, statement) ) ) {
+            response = "Why so negative?";
+        }else if ( (keywordPrecedence(keywords, statement).equals("mother") && (keywordDetection(statement, keywordPrecedence(keywords, statement))))
+                || (keywordPrecedence(keywords, statement).equals("brother") && (keywordDetection(statement, keywordPrecedence(keywords, statement))))
+                || (keywordPrecedence(keywords, statement).equals("sister") && (keywordDetection(statement, keywordPrecedence(keywords, statement))))
+                || (keywordPrecedence(keywords, statement).equals("father") && (keywordDetection(statement, keywordPrecedence(keywords, statement)))))
+        {
+            System.out.println(keywordPrecedence(keywords, statement));
+            System.out.println(keywordDetection(statement, keywordPrecedence(keywords, statement)));
+            response = "Tell me more about your family.";
+        } else if( (keywordPrecedence(keywords, statement).equals("dog") && keywordDetection(statement, keywordPrecedence(keywords, statement)))  || (keywordPrecedence(keywords, statement).equals("cat") && keywordDetection(statement, keywordPrecedence(keywords, statement)) ) ){
+            response = "Tell me more about your pets.";
+        } else if( keywordPrecedence(keywords, statement).equals("Mr.") && keywordDetection(statement,keywordPrecedence(keywords, statement)) ){
+            response = "He sounds like a nice teacher.";
+        } else if(  keywordPrecedence(keywords, statement).equals("Ms.") && keywordDetection(statement,  keywordPrecedence(keywords, statement)) ||  (keywordPrecedence(keywords, statement).equals("Mrs.") && keywordDetection(statement, keywordPrecedence(keywords, statement))) ){
+            response = "She sounds like a nice teacher.";
+        } else {
+            response = getRandomResponse();
+        }
+
+        return response;
+    }
+    
+        
+    public boolean keywordDetection(String statement, String keyword)
+    {
+        int firstLetter = statement.indexOf(keyword);
+        
+        
+    if(firstLetter != -1){
+        if( (firstLetter != 0) && (firstLetter != statement.length() - keyword.length()) && ((statement.substring(firstLetter - 1, firstLetter)).equals(" ") ) && ( (statement.substring(firstLetter + keyword.length(), firstLetter + keyword.length() + 1)).equals(" ") ) ){
+            System.out.println("True");
+            return true;
+        } else if( (firstLetter == 0) && (firstLetter != statement.length() - keyword.length()) && (statement.substring(firstLetter + keyword.length(), firstLetter + keyword.length() + 1 ) ).equals(" ") ) {
+            System.out.println("True");
+            return true;
+        }  else if( (firstLetter == statement.length() - keyword.length()) && !(statement.substring(firstLetter, firstLetter+1).equals(statement.substring(0,1))) && ( (statement.substring(firstLetter - 1, firstLetter) ).equals(" ") ) ) {
+
+            return true;
+        }  else if ( (firstLetter == statement.length() - keyword.length()) && (statement.substring(firstLetter, firstLetter+1).equals(statement.substring(0,1))) ){
+
+            return true;
+        } 
+    }
+
+    return false;
+}
+    
+    public String keywordPrecedence(String[] arr, String statement){
+        //returns the last keyword that occurs in string
+        int max;
+        max = -1;
+        String str = "";
+        for(String string: arr){
+            if( (statement.indexOf(string) > max) && (keywordDetection(statement, string)) ){
+                max = statement.indexOf(string);
+                str = string;
+            }
+        }
+        
+        System.out.println(max);
+        if(max == -1){
+            getRandomResponse();
+            return str;
+        } else {
+           
+            return statement.substring(max, max + str.length() );
+        }
+    }
+
+
+    /**
+     * Pick a default response to use if nothing else fits.
+     * @return a non-committal string
+     */
+    private String getRandomResponse()
+    {
+        final int NUMBER_OF_RESPONSES = 7;
+        double r = Math.random();
+        int whichResponse = (int)(r * NUMBER_OF_RESPONSES);
+        String response = "";
+        
+        if (whichResponse == 0)
+        {
+            response = "Interesting, tell me more.";
+        }
+        else if (whichResponse == 1)
+        {
+            response = "Hmmm.";
+        }
+        else if (whichResponse == 2)
+        {
+            response = "Do you really think so?";
+        }
+        else if (whichResponse == 3)
+        {
+            response = "You don't say.";
+        } else if(whichResponse == 4)
+        {
+            response = "no";
+        } else if(whichResponse == 5)
+        {
+            response = "brother";
+        } else if(whichResponse == 6)
+        {
+            response = "Intriguing...";
+        }
+
+        return response;
+    }
+}
