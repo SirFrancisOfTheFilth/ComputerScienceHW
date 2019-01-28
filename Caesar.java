fbd3fc93912694afc857d582697ac82649a01f2e
SirFrancisOfTheFilth <mtarekegn143@gmail.com>
2019-01-28 14:39:04 -0500

    Create Caesar.java

---
+++ Caesar.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+public class Caesar
+{
+
+    Scanner scan = new Scanner(System.in);
+    String str1 = scan.nextLine();
+    Scanner scan2 = new Scanner(System.in);
+    int integer = scan2.nextInt();
+    
+    public Caesar()
+    {
+        String returnStr = "";
+        
+        char tentative = ' ';
+        for(int i = 0; i < str1.length(); i++){
+            tentative = str1.charAt(i);
+            System.out.println(tentative);
+            
+            if(tentative != ' '){
+                returnStr += (char)( ((int)(tentative)) + (integer % 26) );
+            } else {
+                returnStr += " ";
+            }
+        }
+        
+        System.out.print(returnStr);
+        
+        
+        
+    }
+
+}
